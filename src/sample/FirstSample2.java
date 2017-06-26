2d9ca3b40530078c0ce438c2c9f9506962a65dad
jita <jita@192.168.1.5>
2017-06-26 23:25:54 +0900

    Add FirstSample2.java

---
+++ src/sample/FirstSample2.java
@@ -0,0 +1,19 @@
+package sample;
+
+/** First Sample2 */
+public class FirstSample2 {
+	public static void main(String[] args) {
+
+		FirstSample2 ai = new FirstSample2();
+		ai.run();
+
+	}
+
+	FirstSample2() {
+
+	}
+
+	void run() {
+		System.out.println("Start My Git History.");
+	}
+}
